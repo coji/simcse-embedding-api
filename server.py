5fc6a2d94af3d1cf4820b3b372c8071139eedc7d
coji <coji@techtalk.jp>
2023-08-21 13:05:34 +0900

    fix: omit debug log

--- server.py
+++ server.py
@@ -4,12 +4,8 @@ from sentence_transformers import SentenceTransformer
 model = SentenceTransformer("pkshatech/simcse-ja-bert-base-clcmlp")
 app = FastAPI()
 
+
 @app.get("/embedding")
 def create_embedding(sentence: str):
     embeddings = model.encode(sentence)
-    print(embeddings)
-    return {
-        "sentence": sentence,
-        "embedding": embeddings.tolist()
-    }
-
+    return {"sentence": sentence, "embedding": embeddings.tolist()}
